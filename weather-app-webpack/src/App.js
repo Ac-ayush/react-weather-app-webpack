43321ba01ec8c7bbad5b107fc83356735d0c34f6
Ayush Chouhan <ayushc9977@gmail.com>
2024-01-19 17:06:42 +0530

    First commit

---
+++ weather-app-webpack/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import WeatherForecast from './components/WeatherForecast';
+
+const App = () => {
+  return (
+    <div>
+      <h1>5-Day Weather Forecast</h1>
+      <WeatherForecast />
+    </div>
+  );
+};
+
+export default App;
